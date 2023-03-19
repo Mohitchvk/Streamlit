2faa8f27369b50e08ef232a7d7c74d2ddfbc7961
Mohitchvk <mohit.chvk@gmail.com>
2023-03-18 20:36:22 -0400

    reverted back

--- test.py
+++ test.py
@@ -1,16 +1,3 @@
 import pandas as pd
 import numpy as np
-<<<<<<< HEAD
 import streamlit as st
-
-st.title('Uber pickups in NYC')
-
-st.write("Here's our first attempt at using data to create a table:")
-
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
-=======
-import streamlit as st
->>>>>>> parent of de30e5f (testing streamlit title)
