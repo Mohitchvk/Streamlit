1c899410d1b4afd847f128adac7ca3b27f4cec46
Mohitchvk <mohit.chvk@gmail.com>
2023-03-18 20:33:50 -0400

    Revert "testing streamlit title"
    This reverts commit de30e5fdb3c48aa27b85670eeb0724f9c2646794.

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+<<<<<<< HEAD
 import streamlit as st
 
 st.title('Uber pickups in NYC')
@@ -10,3 +11,6 @@ st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
+=======
+import streamlit as st
+>>>>>>> parent of de30e5f (testing streamlit title)
