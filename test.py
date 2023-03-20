9a052232a6f8185290b03840d144d0555cb5dec6
Mohitchvk <mohit.chvk@gmail.com>
2023-03-20 17:02:24 -0400

    Initialized Containers

--- test.py
+++ test.py
@@ -2,7 +2,12 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
+header=st.container()
+dataset=st.container()
+eda=st.container()
+model_training=st.container()
 
 
+with header:
+    st.title('California House Pricing Analysis!!...')
 
-new3
